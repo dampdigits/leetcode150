f38d321452183c7b09ce4d8a4985ed0031365dee
dampdigits <dampdigits@gmail.com>
2024-04-27 12:13:36 +0530

    created solution for problem11

--- topic1/problem11/Solution.py
+++ topic1/problem11/Solution.py
@@ -1,3 +1,9 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
         
+        for i, num in enumerate(citations):
+            if num >= len(citations) - i:
+                return len(citations) - i
+
+        return 0
